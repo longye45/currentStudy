3c23a30463ce5ddf6cb70450ef93fd29c71ca895
1210395146@qq.com <dhy19940607>
2018-04-17 16:57:17 +0800

    读写锁

---
+++ src/main/java/Cache.java
@@ -0,0 +1,38 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class Cache {
+    static Map<String, Object> map = new HashMap<>();
+    static ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+    static Lock readLock = lock.readLock();
+    static Lock writeLock = lock.writeLock();
+
+    public static final Object get(String key) {
+        readLock.lock();
+        try {
+            return map.get(key);
+        } finally {
+            readLock.unlock();
+        }
+    }
+
+    public static final Object put(String key, Object o) {
+        writeLock.lock();
+        try {
+            return map.put(key, o);
+        } finally {
+            writeLock.unlock();
+        }
+    }
+
+    public static final void clear() {
+        writeLock.unlock();
+        try {
+            map.clear();
+        } finally {
+            writeLock.unlock();
+        }
+    }
+}
