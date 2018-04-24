8834d36b3faec3eea7b073da4f6ecf9a4c5fcb76
1210395146@qq.com <dhy19940607>
2018-04-24 09:44:27 +0800

    原子更新字段引用

---
+++ src/main/java/AtomicIntegerFieldUpdaterTest.java
@@ -0,0 +1,30 @@
+import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
+
+public class AtomicIntegerFieldUpdaterTest {
+
+    private static AtomicIntegerFieldUpdater<User> atomicIntegerFieldUpdater = AtomicIntegerFieldUpdater.newUpdater(User.class, "old");
+
+    public static void main(String[] args) {
+        User conan = new User("conan", 12);
+        System.out.println(atomicIntegerFieldUpdater.getAndIncrement(conan));
+        System.out.println(atomicIntegerFieldUpdater.get(conan));
+    }
+
+    static class User {
+        private String name;
+        public volatile int old;
+
+        public User(String name, int old) {
+            this.name = name;
+            this.old = old;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getOld() {
+            return old;
+        }
+    }
+}
