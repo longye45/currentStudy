6eaf011e0b6fe5bae3350c03aed3e45d5254393b
longy <1210395146@qq.com>
2018-04-30 17:01:36 +0800

    栈排序

---
+++ src/main/java/Stack/OrderByStack.java
@@ -0,0 +1,32 @@
+package Stack;
+
+import java.util.Stack;
+
+public class OrderByStack {
+    private static Stack<Integer> order(Stack<Integer> old) {
+        Stack<Integer> newStack = new Stack<>();
+        while (!old.isEmpty()) {
+            int num = old.pop();
+            if (newStack.isEmpty())
+                newStack.push(num);
+            while (!newStack.isEmpty() && newStack.peek() > num) {
+                old.push(newStack.pop());
+            }
+            newStack.push(num);
+
+        }
+        return newStack;
+    }
+
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < 5; i++) {
+            stack.push(i);
+        }
+        for (int i = 10; i > 5; i--) {
+            stack.push(i);
+        }
+        Stack<Integer> a = order(stack);
+        System.out.println(a);
+    }
+}
